30bfbf0be2f9092c16ceb4bce3613f2755e31b5b
vanhuy2005 <nguyen.van.quang.huy.2105@gmail.com>
2025-09-21 11:27:12 +0700

    feat: add BottomBanner component
    - Add responsive banner with features showcase
    - Integrate BottomBanner to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSellers from '../components/BestSellers' 
+import BottomBanner from '../components/BottomBanner'
 const Home = () => {
   return (
     <div className='mt-10'>
-        <MainBanner></MainBanner>
-        <Categories></Categories>
-        <BestSellers></BestSellers>
+        <MainBanner/>
+        <Categories/>
+        <BestSellers/>
+        <BottomBanner/>
         </div>
   )
 }
